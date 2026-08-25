2980744e271f148d871f3e8b2f365152817f4c5c
SkyShotConner <conneraviation18@gmail.com>
2026-08-25 05:24:24 +0200

    Remove Buy Me A Coffee widget script
    Removed Buy Me A Coffee widget script from the page.

--- script.js
+++ script.js
@@ -355,5 +355,3 @@ navigation
 
 document.querySelector("#year")
     .textContent = new Date().getFullYear();
-
-<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="theskyshotconner" data-description="Support me on Buy me a coffee!" data-message="Thank you for supporting the SkyShotConner Project!" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
