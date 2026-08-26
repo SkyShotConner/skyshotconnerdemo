eaf266025e154c5919472fe56c3cc3270e81ce8d
SkyShotConner <conneraviation18@gmail.com>
2026-08-26 14:03:33 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://mlvbfdyahgszuwtgxmes.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_l4ZBZi1Fxy_AwYVD2KDxiA_FnYNvp2z";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
